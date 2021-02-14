bdf8b348f5a2c9784df29d7fa0c643400668754f
jacksingh570 <74738623+jacksingh570@users.noreply.github.com>
2021-02-13 23:45:43 -0500

    forth commit

--- src/Purchases.java
+++ src/Purchases.java
@@ -12,25 +12,5 @@ public class Purchases {
         shoppingCart.add(groceryItem);
     }
 
-    /**
-     * Adds up the cost for all grocery items in the shopping cart
-     * and returns them
-     */
-    public double getTotalPrice()
-    {
-        double totalPrice = 0;
-//        for ( int i =0; i<shoppingCart.size();i++)
-//            totalPrice = totalPrice + shoppingCart.get(i).getPrice();
 
-        for (GroceryItem groceryItem:shoppingCart)
-            totalPrice = totalPrice + groceryItem.getPrice();
-
-        return totalPrice;
-    }
-
-    public String toString()
-    {
-        return String.format("The shopping cart has %d items worth $%.2f",
-                shoppingCart.size(), getTotalPrice());
-    }
 }
