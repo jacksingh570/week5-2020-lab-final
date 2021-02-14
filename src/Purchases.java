adbe6f9cdda33aebd7b7ced1a77f54cdc49765e0
jacksingh570 <74738623+jacksingh570@users.noreply.github.com>
2021-02-14 00:04:51 -0500

    fifth

--- src/Purchases.java
+++ src/Purchases.java
@@ -12,5 +12,15 @@ public class Purchases {
         shoppingCart.add(groceryItem);
     }
 
+    public double getTotalPrice(){
+        double totalPrice = 0;
+        for(GroceryItem groceryItem:shoppingCart){
+            totalPrice = totalPrice + groceryItem.getPrice();
+        }
+        return totalPrice;
+    }
 
+    public String toString(){
+        return String.format("The shopping cart has %d items worth $%.2f", shoppingCart.size(), getTotalPrice());
+    }
 }
