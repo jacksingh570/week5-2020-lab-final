819ce378ecf6b5aab899dfdeeeb963b45ac5b5f4
jacksingh570 <74738623+jacksingh570@users.noreply.github.com>
2021-02-13 23:39:42 -0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        GroceryItem item1 = new GroceryItem("carrots",4.79,"vegetables");
-        GroceryItem item2 = new GroceryItem("bread",2.89,"bread");
+        GroceryItem item1 = new GroceryItem("mushrooms", 4.20, "vegetables");
+        GroceryItem item2 = new GroceryItem("buns", 3.60, "bread");
 
         Purchases shoppingCart = new Purchases();
         shoppingCart.addGroceryItem(item1);
-        shoppingCart.addGroceryItem(item2);
-        double total = shoppingCart.getTotalPrice();
-        System.out.printf("The total price is: $%.2f%n",total);
-
-        GroceryItem item3 = new GroceryItem("ice cream",7.89,"dairy");
-        shoppingCart.addGroceryItem(item3);
-        shoppingCart.addGroceryItem(new GroceryItem("cheese",12.98,"dairy"));
-        System.out.println(shoppingCart);
+        so
     }
 }
